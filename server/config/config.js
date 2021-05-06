548fcfb50fef61dbaf8df92844c235cd3d85cbfd
Didier <dyder1493@gmail.com>
2021-05-06 13:59:39 +0200

    ajout server to docker compose

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,13 @@
+require("dotenv").config()
+
+const config =
 {
   "development": {
-    "username": "root",
-    "password": "api",
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DEV_BDD_USERNAME,
+    "password": process.env.DEV_BDD_PASSWORD,
+    "database": process.env.DEV_BDD_NAME,
+    "host": process.env.DEV_BDD_HOST,
+    "dialect": process.env.DEV_BDD_DIALECT
   },
   "test": {
     "username": "root",
@@ -21,3 +24,5 @@
     "dialect": "mysql"
   }
 }
+
+module.exports = config
